111b01b973ff29039a1dd609cf39f67accafc98e
TheRad1 <18326@verona.k12.wi.us>
2023-03-31 17:29:07 -0500

    small improvements
    - tells you if already scheduled
    - tells if A+ is closed for that day
    - waits for everything so things don't get messed up

--- scheduler.py
+++ scheduler.py
@@ -11,7 +11,8 @@ import json
 clientInfoJson = open('clientInfo.json')
 clientInfo = json.load(clientInfoJson)
 clientInfoJson.close()
-APlusButtonTimout = 5
+APlusButtonTimout = 2
+sceduleTimout = 20
 
 url = 'https://vasd.powerschool.com/public/home.html'
 sceduleURL = 'https://vasd.powerschool.com/guardian/studentSchedule.html'
@@ -31,18 +32,22 @@ def chooseAPlusses(_client):
 
 	# go to scedule URL
 	driver.get(sceduleURL)
-	WebDriverWait(driver, timeout=APlusButtonTimout).until(lambda d: d.find_element(By.XPATH, "//*[@id=\"content-main\"]/div[3]/div/div[1]/a[2]")) #wait for move week button
+	WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.XPATH, "//*[@id=\"content-main\"]/div[3]/div/div[1]/a[2]")) #wait for move week button
 	driver.find_element(By.XPATH, "//*[@id=\"content-main\"]/div[3]/div/div[1]/a[2]").click() #move forward a week
 	# go forward a week
 	#driver.find_element(By.XPATH, "//a[@ng-click=\"changeWeek(7)\"]").click()
-	for day in range(3, 7):
+	for day in activeDays:
 		try:
 			WebDriverWait(driver, timeout=APlusButtonTimout).until(lambda d: d.find_element(By.XPATH, "//*[@id=\"content-main\"]/div[3]/div/table/tbody/tr[2]/td[" + str(day) + "]/button")) #wait for A+ button
 			driver.find_element(By.XPATH, "//*[@id=\"content-main\"]/div[3]/div/table/tbody/tr[2]/td[" + str(day) + "]/button").click()	#click A+ button
-			WebDriverWait(driver, timeout=buttonTimout).until(lambda d: d.find_element(By.XPATH, "//*[@id=\"sessionPicker\"]/table[2]/tbody/tr[15]/td[1]/a")) #wait for double lunch
+			WebDriverWait(driver, timeout=sceduleTimout).until(lambda d: d.find_element(By.XPATH, "//*[@id=\"sessionPicker\"]/table[2]/tbody/tr[15]/td[1]/a")) #wait for double lunch
 			driver.find_element(By.XPATH, "//*[@id=\"sessionPicker\"]/table[2]/tbody/tr[15]/td[1]/a").click() #schedule double lunch
 		except:
-			print("A+ for day with ID " + str(day) + " are not open")
+			try:
+				driver.find_element(By.XPATH, "//*[@id=\"content-main\"]/div[3]/div/table/tbody/tr[2]/td[" + str(day) + "]/div/button")	#check if something was already scheduled
+				print("A+ already selected for day " + str(day))
+			except:
+				print("A+ is closed for day " + str(day))
 	
 	driver.implicitly_wait(5)
 	time.sleep(5)
@@ -57,9 +62,10 @@ def chooseAPlusses(_client):
 
 #starting
 chrome_options = Options()
-#chrome_options.add_argument('--no-sandbox')
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging']) #gets rid of annoyng debug messages from chrome
+#chrome_options.add_argument('--no-sandbox') 
 #chrome_options.add_argument("--headless") #uncomment if you dont want GUI
 #//chrome_options.add_argument('--disable-dev-shm-usage')
-
+activeDays = clientInfo['activeDays']
 for client in clientInfo['clients']:
 	chooseAPlusses(client)
